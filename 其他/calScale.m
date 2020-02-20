d9d9e8cb166ba7d576cb847adf93af68fd2af2cb
‘bibe <hyj1997wshhz@163.com>
2020-02-20 21:01:32 +0800

    update 更新其他参数

--- 其他/calScale.m
+++ 其他/calScale.m
@@ -4,18 +4,21 @@
 % anno/
 % scaledata/
 % calScale.m
+% 如果新增参数，需要将 scaledata/ 内原有文件删除
+
+clc;clear;close all;
+% DTB70 UAV20L UAV123 UAV123_10fps UAVDT
+datasetName = 'UAV123_10fps';
+
+% 相关文件夹
+annoFolder = ['./anno/' datasetName '/'];
+resultFolder = ['./scaledata/' datasetName '/'];
 
 % 读取后namelist 的格式为
 % name -- filename
 % date -- modification date
 % bytes -- number of bytes allocated to the file
 % isdir -- 1 if name is a directory and 0 if not
-clc;clear;close all;
-% DTB70 UAV20L UAV123 UAV123_10fps UAVDT
-datasetName = 'UAV123_10fps';
-% folder = ['./' datasetName '/'];
-annoFolder = ['./anno/' datasetName '/'];
-resultFolder = ['./scaledata/' datasetName '/'];
 namelist = dir([annoFolder '*.txt']);
 len = length(namelist);
 for i = 1:len
@@ -84,7 +87,7 @@ if visStep
 end
 
 %% 统计h>w的情况
-calFrame = 0; % 是否计算并输出
+calFrame = 1; % 是否计算并输出
 calNum = len; % 写入 len
 table_name = [datasetName '_scale_data.xlsx'];
 txt_name = [datasetName '_scale_data.txt'];
@@ -113,30 +116,50 @@ if calFrame
         seqName = file_name{i}(1:end-4);
         % 初始比例
         ratio0 = read_data(1,4) / read_data(1,3);
-        % wFrame >= hFrame
+        % 序列wFrame >= hFrame
+        if wFrame >= hFrame
+            wGeqh='1'; wLh='0';
+        % 序列wFrame < hFrame
+        else
+            wGeqh='0'; wLh='1';
+        end
         % maxW
+        maxW = max(scaleChange(:,1));
         % minW
+        minW = min(scaleChange(:,1));
         % maxH
+        maxH = max(scaleChange(:,2));
         % minH
+        minH = min(scaleChange(:,2));
         % TO DO 其他新增参数
+        % 新增参数需要改 write_data, T=table(...) T.Properties.VariableNames
+        
+        % 写入txt数据格式
         write_data = [seqName ',' num2str(wFrame) ',' num2str(hFrame) ',' ...
-            num2str(eFrame) ',' num2str(nanFrame) ',' num2str(ratio0)];
+            num2str(eFrame) ',' num2str(nanFrame) ',' num2str(ratio0) ',' ...
+            wGeqh ',' wLh, ',' ...
+            num2str(maxW) ',' num2str(minW) ',' ...
+            num2str(maxH) ',' num2str(minH)];
         % 关于文件夹相关判定
         if exist(resultFolder)==0 %该文件夹不存在，则直接创建
             mkdir(resultFolder);
-        else %该文件夹存在，则先删除再创建
-            rmdir(resultFolder, 's'); %该文件夹中有没有文件均可
-            mkdir(resultFolder);
+%         else %该文件夹存在，则先删除再创建
+%             rmdir(resultFolder, 's'); %该文件夹中有没有文件均可
+%             mkdir(resultFolder);
         end
-        fid=fopen([resultFolder txt_name],'a'); % validation training
+        fid=fopen([resultFolder txt_name],'a+'); % validation training
         fprintf(fid,'%s\n',write_data);
         fclose(fid);
     end
+    % txt转xlsx/csv
     dt=importdata([resultFolder txt_name]); 
     T=table(dt.rowheaders,dt.data(:,1),dt.data(:,2),...
-        dt.data(:,3),dt.data(:,4),dt.data(:,5));
-    T.Properties.VariableNames = {'Seq','wbigger','hbigger','whequal','nan','initial_ratio'};
+        dt.data(:,3),dt.data(:,4),dt.data(:,5),...
+        dt.data(:,6),dt.data(:,7),dt.data(:,8),...
+        dt.data(:,9),dt.data(:,10),dt.data(:,11));
+    T.Properties.VariableNames = {'seqName','wBiggerFrames','hBiggerFrames','whEqualFrames','nanFrames','initialRatio'...
+        'wDominance','hDominance','maxW','minW','maxH','minH'};
     writetable(T,[resultFolder table_name]);
 end
 
-%% 计算尺度变化方格
+%% TODO 计算尺度变化方格
